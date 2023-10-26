968f81d8a1ae90f53a93b62a1411fc3a18ed6cbb
Guillermo Alvarez <gjalvarez09@gmail.com>
2023-10-26 02:37:47 +0000

    feat: rules modal

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,6 +1,9 @@
 import { Application } from "@hotwired/stimulus"
+import { Modal } from "tailwindcss-stimulus-components"
 
 const application = Application.start()
+
+application.register('modal', Modal)
 
 // Configure Stimulus development experience
 application.debug = false
